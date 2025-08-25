45bb8f324017420537d6ed3a983481f03f132377
souleimarejeb <souleima.Rejeb@esprit.tn>
2025-08-25 14:23:56 +0200

    chore: implement Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   const config = new DocumentBuilder()
+    .setTitle('CAB Api')
+    .setDescription('Cab Game API')
+    .setVersion('1.0')
+    .addTag('cab')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
