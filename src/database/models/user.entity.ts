c6083ae157f7361c0a04907d2b63bb21a586cf3e
souleimarejeb <souleima.Rejeb@esprit.tn>
2025-08-26 17:02:33 +0200

    update(entity): minor adjustments and fixes

--- src/database/models/user.entity.ts
+++ src/database/models/user.entity.ts
@@ -11,23 +11,23 @@ import { ApiProperty } from '@nestjs/swagger';
 export class UserEntity extends BaseEntity {
 
   @ApiProperty()
-  @Column({ type: 'varchar', length: '250', nullable: true })
+  @Column({ type: 'varchar', length: '250',default:'N/A' })
   name: string;
 
   @ApiProperty()
-  @Column({ type: 'varchar', length: '1250', nullable: true })
+  @Column({ type: 'varchar', length: '1250', default:'N/A' })
   last_name: string;
 
   @ApiProperty()
-  @Column({ type: 'varchar', length: '1250', nullable: true, unique:true})
+  @Column({ type: 'varchar', length: '1250',  unique:true})
   username: string;
   
   @ApiProperty()
-  @Column({ type: 'varchar', length: '250', nullable: true, unique:true})
+  @Column({ type: 'varchar', length: '250', unique:true})
   email: string;
 
   @ApiProperty()
-  @Column({ type: 'varchar', length: '250', nullable: true })
+  @Column({ type: 'varchar', length: '250' })
   authentication_id: string;
 
   @ApiProperty()
