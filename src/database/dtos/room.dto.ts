e84e1585ce768e76f3b68d0b0dee42b622f3dede
souleimarejeb <souleima.Rejeb@esprit.tn>
2025-08-28 16:56:08 +0200

    feat(dto): add validation decorators

--- src/database/dtos/room.dto.ts
+++ src/database/dtos/room.dto.ts
@@ -1,22 +1,33 @@
 import { ApiProperty, PartialType } from "@nestjs/swagger";
 import { UserEntity } from "../models/user.entity";
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString, Length } from 'class-validator';
+
 
 export class CreateRoomDto {
     @ApiProperty()
+    @IsString({ message: 'winner_id must be a string' })
+    @IsOptional()
     winner_id: string;
 
+
     @ApiProperty()
+    @IsOptional()
     tries: number;
 
     @ApiProperty()
+    @IsOptional()
     score: number;
 }
 export class UpdateRoomDto extends PartialType(CreateRoomDto) {
 
     @ApiProperty()
+    @IsString({ message: 'Player_id must be a string' })
+    @IsOptional()
     player_1: UserEntity;
 
     @ApiProperty()
+    @IsString({ message: 'Player_id must be a string' })
+    @IsOptional()
     player_2: UserEntity;
 
 }
